0b550d279c2575077b5a49daf4614880084ad558
AbstarctElement <fcukthepop@gmail.com>
2017-11-20 11:37:57 +0300

    Initial commit

---
+++ src/main/java/com/epmbdas/services/GroupRoleCleaner.java
@@ -0,0 +1,36 @@
+package com.epmbdas.services;
+
+import com.atlassian.configurable.ObjectConfiguration;
+import com.atlassian.configurable.ObjectConfigurationException;
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.security.roles.ProjectRoleManager;
+import com.atlassian.jira.service.AbstractService;
+import com.opensymphony.module.propertyset.PropertySet;
+import java.util.Arrays;
+import java.util.List;
+
+public class GroupRoleCleaner extends AbstractService {
+    private String input;
+
+    public ObjectConfiguration getObjectConfiguration() throws ObjectConfigurationException {
+        return getObjectConfiguration("GroupRoleCleaner", "com/epmbdas/services/GroupRoleCleaner.xml", null);
+    }
+
+    public void run() {
+        if (input != null && input.length() > 0) {
+
+            List<String> groups = Arrays.asList(input.split(","));
+            ProjectRoleManager projectRoleManager = (ProjectRoleManager) ComponentAccessor.getComponentOfType(ProjectRoleManager.class);
+
+            for (String group : groups) {
+                projectRoleManager.removeAllRoleActorsByNameAndType(group.trim(), com.atlassian.jira.security.roles.ProjectRoleActor.GROUP_ROLE_ACTOR_TYPE);
+            }
+        }
+    }
+
+    @Override
+    public void init(PropertySet props) throws ObjectConfigurationException {
+        super.init(props);
+        if (hasProperty("Affected Groups")) input = getProperty("Affected Groups");
+    }
+}
